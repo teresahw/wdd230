0302d9fc6e5a00b01ec015ae07b833afac177b07
teresahw <teresahw@byui.edu>
2022-04-25 22:00:14 -0600

    Update with javascript date

---
+++ main.js
@@ -0,0 +1,4 @@
+// toLocaleDateString
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
