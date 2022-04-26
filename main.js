b775d2c64f44f9728c56037137b6a34c8dd701da
teresahw <teresahw@byui.edu>
2022-04-26 14:26:20 -0600

    add normalize.css

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-// toLocaleDateString
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-
+alert(document.lastModified);
