90413a62cdb2c88890898cfef30690aaf4e3375a
teresahw <teresahw@byui.edu>
2022-06-02 15:59:57 -0600

    test windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,12 +1,12 @@
 tempurature = 35;
 windSpeed = 15;
 
-windChill = document.getElementById('wind-chill');
+outcome = document.getElementById('wind-chill');
 
 if ((tempurature <= 50) && (windSpeed > 3)){
     let windChill = 35.74 + 0.6215 * tempurature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * tempurature * Math.pow(windSpeed, 0.16);
-    windChill.textContent = windChill;
+    outcome.textContent = windChill;
 }
 else {
-    windChill.textContent = "N/A";
+    outcome.textContent = "N/A";
 }
