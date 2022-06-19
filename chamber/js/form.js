b06c71a0bbfe5c0ff87d5d65821bca2b0cb266a4
teresahw <teresahw@byui.edu>
2022-06-18 22:16:19 -0600

    join

--- chamber/js/form.js
+++ chamber/js/form.js
@@ -1,2 +1,17 @@
 let dayTimeValue = document.getElementById("dayTime")
 dayTimeValue.value = new Date();
+
+function validate(){
+    var exp = /^[A-Za-z\s\-]+$/;
+    var position = document.myform.position.value;
+    var result = exp.test(position);
+
+    if(result){
+        alert("validation successful");
+    }else{
+        alert("validation failed");
+        document.getElementById("error").innerHTML= "Please enter letters, hyphens and spaces only";
+        document.myform.position.focus();
+        return false;
+    }
+}
