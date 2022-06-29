0f7909c57c8776d5715c11b0e06baba58db51375
teresahw <teresahw@byui.edu>
2022-06-29 16:07:17 -0600

    update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,7 +1,7 @@
 const requestURL = 'JSON/data.json';
 const cards = document.querySelector('.cards');
 
-async function getDirectory() {
+async function getDirectory(requestURL) {
     let response = await fetch(requestURL)
     if (response.ok) {
         let data = await response.json();
@@ -22,12 +22,12 @@ async function buildCards(data) {
         let phone = document.createElement('p');
         let img = document.createElement('img');
 
-        business.innerHTML = `${data.business}`;
-        membership.innerHTML = `Type if Membership: ${data.member}`;
-        founder.innerHTML = `Founder(s): ${data.founder}`;
-        address.innerHTML = `Address: ${data.address}`;
-        phone.innerHTML = `Phone Number: ${data.phone}`;
-        img.setAttribute('src' , `${data.img}`);
+        business.innerHTML = `${directory.business}`;
+        membership.innerHTML = `Type if Membership: ${directory.member}`;
+        founder.innerHTML = `Founder(s): ${directory.founder}`;
+        address.innerHTML = `Address: ${directory.address}`;
+        phone.innerHTML = `Phone Number: ${directory.phone}`;
+        img.setAttribute('src' , `${directory.img}`);
 
         card.append(business);
         card.append(membership);
@@ -39,4 +39,4 @@ async function buildCards(data) {
         cards.append(card);
     });
 }
-getDirectory();
+buildCards();
