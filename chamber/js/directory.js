57a0b353a2d2baf63bed76db3cd3774a8224179d
teresahw <teresahw@byui.edu>
2022-06-29 17:48:55 -0600

    directory

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -16,27 +16,28 @@ function buildCards(data) {
     data.directory.forEach(index => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let membership = document.createElement('h3');
-        let founder = document.createElement('h4');
-        let address = document.createElement('p');
+        let h3 = document.createElement('h3');
+        let h4 = document.createElement('h4');
+        let p = document.createElement('p');
         let phone = document.createElement('p');
         let img = document.createElement('img');
 
-        h2.innerHTML = `${index.business}`;
-        membership.innerHTML = `Type if Membership: ${index.member}`;
-        founder.innerHTML = `Founder(s): ${index.founder}`;
-        address.innerHTML = `Address: ${index.address}`;
+        h2.innerHTML = index.business;
+        h3.innerHTML = `Type if Membership: ${index.membership}`;
+        h4.innerHTML = `Founder(s): ${index.founder}`;
+        p.innerHTML = `Address: ${index.address}`;
         phone.innerHTML = `Phone Number: ${index.phone}`;
         img.setAttribute('src' , `${index.img}`);
 
-        card.append(business);
-        card.append(membership);
-        card.append(founder);
-        card.append(address);
+        card.append(h2);
+        card.append(h3);
+        card.append(h4);
+        card.append(p);
         card.append(phone);
         card.append(img);
 
         cards.append(card);
+     
     });
 }
 getDirectory();
