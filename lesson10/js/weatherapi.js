31da8382621bc3c3bf80972814bfb306c61553fc
teresahw <teresahw@byui.edu>
2022-06-30 13:16:59 -0600

    weather API

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=88c068cdaad4fa1274ba537aca974610&units=Imperial`;
+
+
+apiFetch(url);
+
+async function apiFetch(apiURL) {
+   try { 
+    const response = await fetch(apiURL);
+    if (response.ok) {
+        const data = await response.json();
+        //console.log(data);
+        displayResults(data);
+    } else{
+        throw Error(await response.text());
+    }
+} catch (error) {
+    console.log(error);
+}
+}
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(1);
+
+    const imagesrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', imagesrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = desc;
+}
