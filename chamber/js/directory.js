a5a94ce39b6f4f0b2fcd38a0b18c3660bebf74b3
teresahw <teresahw@byui.edu>
2022-07-01 18:45:42 -0600

    directory css

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -43,3 +43,20 @@ function buildCards(data) {
 }
 getDirectory();
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("cards");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); 
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("cards");
+}
