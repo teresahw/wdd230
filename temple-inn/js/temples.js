ea15fe671c721cb87bbcc1af1c344f93582d30bf
teresahw <teresahw@byui.edu>
2022-07-18 21:54:21 -0600

    temples

--- temple-inn/js/temples.js
+++ temple-inn/js/temples.js
@@ -1,35 +1,41 @@
 const requestURL = 'https://teresahw.github.io/wdd230/temple-inn/JSON/data.json';
 const cards = document.querySelector('.cards');
 
-async function prophets() {
-    let response = await fetch(requestURL);
+
+async function getTemples() {
+    let response = await fetch(requestURL)
     if (response.ok) {
         let data = await response.json();
-        buildProphetCards(data);
+        buildCards(data);
     }else {
         throw Error(response.statusText);
     }
 }
 
-async function buildProphetCards(data) {
-    data.prophets.forEach(prophet => {
+function buildCards(data) {
+    
+    data.temples.forEach(index => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let date = document.createElement('p');
-        let place = document.createElement('p');
         let img = document.createElement('img');
+        let h3 = document.createElement('h3');
+        let h4 = document.createElement('h4');
+        let p = document.createElement('p');
 
-        h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-        date.innerHTML = `Date of Birth: ${prophet.birthdate}`;
-        place.innerHTML = `Location of Birth: ${prophet.birthplace}`;
-        img.setAttribute('src' , `${prophet.imageurl}`);
+        h2.innerHTML = index.temple;
+        img.setAttribute('src' , `${index.image}`);
+        h3.innerHTML = index.address;
+        h4.innerHTML = `Phone: ${index.phone}`;
+        p.innerHTML = `History: ${index.history}`;
 
         card.append(h2);
-        card.append(date);
-        card.append(place);
         card.append(img);
+        card.append(h3);
+        card.append(h4);
+        card.append(p);
 
         cards.append(card);
+     
     });
 }
-prophets(requestURL);
+getTemples();
